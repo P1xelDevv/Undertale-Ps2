a879e99177409df205e7fc3e86d945081ab997fe
Bernardev3 <118381885+Bernardev3@users.noreply.github.com>
2023-11-19 12:48:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-//Undertale de ps2 Projeto
+const player = {
+	state: "idle",
+	x: 0,
+	y: 0,
+	sprites: {
+		movingUp: new Image("img/player_up.png"),
+		movingDown: new Image("img/player_down.png"),
+		movingLeft: new Image("img/player_left.png"),
+		movingRight: new Image("img/player_right.png"),
+		idle: new Image("img/player_idle.png")
+	}
+}
+
+let pad;
+
+while (true) {
+	pad = Pads.get();
+
+	switch (true) {
+		case Pads.check(pad, Pads.UP):
+			player.state = "movingUp"
+			player.y -= 10;
+			break;
+		case Pads.check(pad, Pads.DOWN):
+			player.state = "movingDown"
+			player.y += 10;
+			break;
+		case Pads.check(pad, Pads.LEFT):
+			player.state = "movingLeft"
+			player.x -= 10;
+			break;
+		case Pads.check(pad, Pads.RIGHT):
+			player.state = "movingRight"
+			player.x += 10;
+			break;
+		default:
+			player.state = "idle";
+			break;
+	}
+
+	Screen.clear(Color.new(0, 0, 0));
+	player.sprites[player.state].draw(player.x, player.y);
+	Screen.flip();
+}
